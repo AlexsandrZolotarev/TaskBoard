17d57b40fe43babcb19619fccade35965384ab04
Alexsandr Zolotarev <380788-500@mail.ru>
2025-05-10 19:34:40 +0300

    feat(auth): add navigation link from login to register form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,8 @@
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import { Link } from "react-router-dom";
+import { InputField } from "../UI/InputField";
 
 const schema = yup.object({
   email: yup.string().email("Некорректный email").required("Email обязателен"),
@@ -29,40 +31,36 @@ export const Login = () => {
     <div className="max-w-md mx-auto bg-white p-6 rounded shadow-md">
       <h2 className="text-xl font-bold mb-4">Вход</h2>
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-        <div>
-          <label className="block mb-1 font-medium">Email</label>
-          <input
-            {...register("email")}
-            type="email"
-            autoComplete="email"
-            className={`w-full border rounded px-3 py-2 ${
-              errors.email ? "border-red-500" : "border-gray-300"
-            }`}
-          />
-          {errors.email && (
-            <p className="text-red-600 text-sm">{errors.email.message}</p>
-          )}
-        </div>
+        <InputField
+          label="Email"
+          type="email"
+          autoComplete="email"
+          placeholder="Введите email"
+          {...register("email")}
+          error={errors.email?.message}
+        />
 
-        <div>
-          <label className="block mb-1 font-medium">Пароль</label>
-          <input
-            {...register("password")}
-            autoComplete="current-password"
-            type="password"
-            className={`w-full border rounded px-3 py-2 ${
-              errors.email ? "border-red-500" : "border-gray-300"
-            }`}
-          />
-          {errors.password && (
-            <p className="text-red-600 text-sm">{errors.password.message}</p>
-          )}
-        </div>
-
-        <button type="submit"  className="bg-blue-600 hover:bg-blue-700 text-white w-full py-2 rounded font-medium transition">
+        <InputField
+          label="Пароль"
+          type="password"
+          autoComplete="current-password"
+          placeholder="Введите password"
+          {...register("password")}
+          error={errors.password?.message}
+        />
+        <button
+          type="submit"
+          className="bg-blue-600 hover:bg-blue-700 text-white w-full py-2 rounded font-medium transition"
+        >
           Войти
         </button>
       </form>
+      <p className="text-sm text-center text-gray-600">
+        Нет аккаунта?{" "}
+        <Link to="/register" className="text-blue-600 hover:underline">
+          Зарегистрироваться
+        </Link>
+      </p>
     </div>
   );
 };
