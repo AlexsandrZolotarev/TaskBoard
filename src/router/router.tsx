032252e733e1bcb4bf282dd028fc7d325f32ad18
Alexsandr Zolotarev <380788-500@mail.ru>
2025-05-11 16:00:55 +0300

    feat(auth): protect home route with PrivateRoute and redirect unauthenticated users

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,18 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "../pages/Home";
-import { Login } from "../pages/Login";
-import { Register } from "../pages/Register";
-import MainLayout from "../pages/MainLayout";
-
+import { PrivateRoute } from "./PrivateRoute";
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <MainLayout />,
+    element: <PrivateRoute />,
     children: [
       { index: true, element: <Home /> },
-      { path: 'login', element: <Login /> },
-      { path: 'register', element: <Register /> },
     ],
   },
 ])
