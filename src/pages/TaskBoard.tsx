fab911a667bae51f49086bce2bbc9dd4bb991195
Alexsandr Zolotarev <380788-500@mail.ru>
2025-05-12 12:54:29 +0300

    feat(tasks): user-specific task loading and saving with  Redux

--- src/pages/TaskBoard.tsx
+++ src/pages/TaskBoard.tsx
@@ -1,5 +1,8 @@
 import { TrashIcon } from "@heroicons/react/24/outline";
 import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import type { RootState } from "../store";
+import { saveTasks } from "../store/slices/usersSlice";
 
 type TaskType = {
   id: number;
@@ -8,14 +11,13 @@ type TaskType = {
 };
 
 export const TaskBoard = () => {
-  const [tasks, setTasks] = useState<TaskType[]>(() => {
-    const saved = localStorage.getItem("tasks");
-    try {
-      return saved ? JSON.parse(saved) : [];
-    } catch {
-      return [];
-    }
-  });
+  const dispatch = useDispatch();
+  const email = useSelector((state: RootState) => state.auth.email);
+  const user = useSelector((state: RootState) =>
+    state.users.find((user) => user.email === email)
+  );
+
+  const [tasks, setTasks] = useState<TaskType[] | []>(user?.tasks || []);
   const [filter, setFilter] = useState<"all" | "active" | "completed">("all");
   const [value, setValue] = useState<string>("");
 
@@ -26,9 +28,6 @@ export const TaskBoard = () => {
       )
     );
   };
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
   const AddTask = () => {
     const trimmed = value.trim();
     if (!trimmed) return;
@@ -42,65 +41,75 @@ export const TaskBoard = () => {
     const value = e.target.value as typeof filter;
     setFilter(value);
   };
+
   const filteredTasks = tasks.filter((task) => {
     if (filter === "active") return !task.isDone;
     if (filter === "completed") return task.isDone;
     return true;
   });
-  return (
-    <div>
+  useEffect(() => {
+    if (email) {
+      dispatch(saveTasks({ email, tasks }));
+    }
+  }, [tasks]);
+  if (!user) {
+    return null;
+  } else
+    return (
       <div>
-        <label>
-          <input
-            type="text"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-            onKeyDown={(e) => e.key === "Enter" && AddTask()}
-            placeholder="Новая задача..."
-            className="border px-3 py-2 rounded w-full"
-          />
-        </label>
-        <button onClick={() => AddTask()}>Добавить Задачу</button>
-      </div>
-      <div>
-        <label htmlFor="filter-select"></label>
-        <select
-          value={filter}
-          name="filter"
-          id="filter-select"
-          onChange={handleFilterChange}
-        >
-          <option value="">-- Фильтр --</option>
-          <option value="all">Все</option>
-          <option value="active">Активные</option>
-          <option value="completed">Завершённые</option>
-        </select>
-      </div>
-      <ul className="space-y-2">
-        {filteredTasks.map((task) => (
-          <li
-            key={task.id}
-            className="bg-white p-4 rounded shadow flex items-center justify-between"
-          >
-            <label className="flex items-center gap-3 cursor-pointer">
-              <input
-                type="checkbox"
-                checked={task.isDone}
-                onChange={() => toggleDone(task.id)}
-                className="w-4 h-4"
-              />
-              <span className={task.isDone ? "line-through text-gray-400" : ""}>
-                {task.title}
-              </span>
-            </label>
-
-            <TrashIcon
-              className="w-5 h-5 text-red-500 hover:text-red-700 cursor-pointer"
-              onClick={() => DeleteTask(task.id)}
+        <div>
+          <label>
+            <input
+              type="text"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              onKeyDown={(e) => e.key === "Enter" && AddTask()}
+              placeholder="Новая задача..."
+              className="border px-3 py-2 rounded w-full"
             />
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+          </label>
+          <button onClick={() => AddTask()}>Добавить Задачу</button>
+        </div>
+        <div>
+          <label htmlFor="filter-select"></label>
+          <select
+            value={filter}
+            name="filter"
+            id="filter-select"
+            onChange={handleFilterChange}
+          >
+            <option value="all">Все</option>
+            <option value="active">Активные</option>
+            <option value="completed">Завершённые</option>
+          </select>
+        </div>
+        <ul className="space-y-2">
+          {filteredTasks.map((task) => (
+            <li
+              key={task.id}
+              className="bg-white p-4 rounded shadow flex items-center justify-between"
+            >
+              <label className="flex items-center gap-3 cursor-pointer">
+                <input
+                  type="checkbox"
+                  checked={task.isDone}
+                  onChange={() => toggleDone(task.id)}
+                  className="w-4 h-4"
+                />
+                <span
+                  className={task.isDone ? "line-through text-gray-400" : ""}
+                >
+                  {task.title}
+                </span>
+              </label>
+
+              <TrashIcon
+                className="w-5 h-5 text-red-500 hover:text-red-700 cursor-pointer"
+                onClick={() => DeleteTask(task.id)}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
 };
